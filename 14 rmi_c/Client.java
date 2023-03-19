a213dc739f206b00915d76febbe9821f2dca4b19
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-03-19 09:29:20 -0700

    RMI protocol

---
+++ 14 rmi_c/Client.java
@@ -0,0 +1,20 @@
+import java.rmi.registry.LocateRegistry; 
+import java.rmi.registry.Registry;  
+
+public class Client {  
+   private Client() {}  
+   public static void main(String[] args) {  
+      try {  
+         Registry registry = LocateRegistry.getRegistry(null); 
+    
+         Hello stub = (Hello) registry.lookup("Hello"); 
+    
+         stub.printMsg(); 
+         
+         // System.out.println("Remote method invoked");
+      } catch (Exception e) {
+         System.err.println("Client exception: " + e.toString()); 
+         e.printStackTrace(); 
+      } 
+   } 
+}
